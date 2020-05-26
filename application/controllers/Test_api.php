1a0d0b11008d1f62fbe9ba59c3ef71a38b0a07d1
Haris khan <harisali.khan@thetasp.com>
2020-05-26 19:27:57 +0800

    initial seeding

---
+++ application/controllers/Test_api.php
@@ -0,0 +1,116 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_api extends CI_Controller {
+
+	function index() {
+		$this->load->view('api_view');
+	}
+
+	function action() {
+		if($this->input->post('data_action')) {
+			$data_action = $this->input->post('data_action');
+
+			if($data_action == "Delete") {
+				$api_url = "http://localhost/codeigniter-api/api/delete";
+
+				$form_data = array(
+					'id'		=>	$this->input->post('user_id')
+				);
+
+				$client = curl_init($api_url);
+				curl_setopt($client, CURLOPT_POST, true);
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($client);
+				curl_close($client);
+				echo $response;
+			}
+
+			if($data_action == "Edit") {
+				$api_url = "http://localhost/codeigniter-api/api/update";
+
+				$form_data = array(
+					'first_name'		=>	$this->input->post('first_name'),
+					'last_name'			=>	$this->input->post('last_name'),
+					'id'				=>	$this->input->post('user_id')
+				);
+
+				$client = curl_init($api_url);
+				curl_setopt($client, CURLOPT_POST, true);
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($client);
+				curl_close($client);
+				echo $response;
+			}
+
+			if($data_action == "fetch_single") {
+				$api_url = "http://localhost/codeigniter-api/api/fetch_single";
+
+				$form_data = array(
+					'id'		=>	$this->input->post('user_id')
+				);
+
+				$client = curl_init($api_url);
+				curl_setopt($client, CURLOPT_POST, true);
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($client);
+				curl_close($client);
+				echo $response;
+			}
+
+			if($data_action == "Insert") {
+				$api_url = "http://localhost/codeigniter-api/api/insert";
+			
+				$form_data = array(
+					'first_name'		=>	$this->input->post('first_name'),
+					'last_name'			=>	$this->input->post('last_name')
+				);
+
+				$client = curl_init($api_url);
+				curl_setopt($client, CURLOPT_POST, true);
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($client);
+				curl_close($client);
+				echo $response;
+			}
+
+			if($data_action == "fetch_all") {
+				$api_url = "http://localhost/codeigniter-api/api";
+
+				$client = curl_init($api_url);
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($client);
+				curl_close($client);
+				$result = json_decode($response);
+				$output = '';
+				if(count($result) > 0) {
+					foreach($result as $row) {
+						$output .= '
+						<tr>
+							<td>'.$row->first_name.'</td>
+							<td>'.$row->last_name.'</td>
+							<td><butto type="button" name="edit" class="btn btn-warning btn-xs edit" id="'.$row->id.'">Edit</button></td>
+							<td><button type="button" name="delete" class="btn btn-danger btn-xs delete" id="'.$row->id.'">Delete</button></td>
+						</tr>
+						';
+					}
+				}
+				else {
+					$output .= '
+					<tr>
+						<td colspan="4" align="center">No Data Found</td>
+					</tr>
+					';
+				}
+				echo $output;
+			}
+		}
+	}
+	
+}
+
+?>
---
+++ application/models/Api_model.php
@@ -0,0 +1,35 @@
+<?php
+class Api_model extends CI_Model
+{
+	function fetch_all() {
+		$this->db->order_by('id', 'DESC');
+		return $this->db->get('tbl_sample');
+	}
+
+	function insert_api($data) {
+		$this->db->insert('tbl_sample', $data);
+	}
+
+	function fetch_single_user($user_id) {
+		$this->db->where('id', $user_id);
+		$query = $this->db->get('tbl_sample');
+		return $query->result_array();
+	}
+
+	function update_api($user_id, $data) {
+		$this->db->where('id', $user_id);
+		$this->db->update('tbl_sample', $data);
+	}
+
+	function delete_single_user($user_id) {
+		$this->db->where('id', $user_id);
+		$this->db->delete('tbl_sample');
+		if($this->db->affected_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+?>
